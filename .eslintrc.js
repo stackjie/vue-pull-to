0c006b033e1b7c059c59bdfc6baffca31815af6c
stackjie <stackjie@gmail.com>
2017-04-02 22:38:31 +0800

    modify eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "airbnb/base",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+};
