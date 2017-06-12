2f11afbbfcdedd50e43f7db08568ab135f95461a
stackjie <stackjie@gmail.com>
2017-06-13 00:42:09 +0800

    Update example

--- example/pages/Home.vue
+++ example/pages/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>hello</h1>
+  <div>
+    <h1>hello</h1>
+    <fast-scroll></fast-scroll>
+  </div>
 </template>
 
 <script type="text/babel">
+  import FastScroll from '@/vue-fast-scroll.vue'
   export default {
     name: 'home',
+    components: {
+      FastScroll
+    },
     data () {
       return {
       }
