c3d5c21439c0c73bb991cb7e46ae4a990be78da4
stackjie <stackjie@gmail.com>
2017-06-18 22:49:27 +0800

    重构

--- example/pages/Home.vue
+++ example/pages/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="wrapper">
-    <fast-scroll></fast-scroll>
+    <fast-scroll>
+      <div class="list-view">
+      </div>
+    </fast-scroll>
   </div>
 </template>
 
 <style>
   .wrapper {
     height: 100%;
+  }
+
+  .list-view {
+    z-index: 999;
     background: #fff;
+    height: 569px;
   }
 </style>
 
 <script type="text/babel">
-  import FastScroll from '@/vue-fast-scroll.vue'
+  import FastScroll from '@/vue-fast-scroll_bak.vue'
 
   export default {
     name: 'home',
