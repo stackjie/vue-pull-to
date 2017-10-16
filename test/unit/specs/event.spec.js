75c330833060972f540f3329fb5ff80ebf122b23
stackjie <stackjie@gmail.com>
2017-10-16 21:51:24 +0800

    test: update event test for unit test

--- test/unit/specs/event.spec.js
+++ test/unit/specs/event.spec.js
@@ -4,7 +4,7 @@ import PullTo from '../../../src';
 
 describe('event', () => {
   it('top pull', (done) => {
-    let res = false;
+    let res;
     const vm = createVue({
       template: `
         <div style="height: 500px">
@@ -13,8 +13,8 @@ describe('event', () => {
       `,
       components: {PullTo},
       methods: {
-        onTopPull() {
-          res = true;
+        onTopPull(e) {
+          res = e;
         }
       }
     }, true);
@@ -29,13 +29,13 @@ describe('event', () => {
     elem.dispatchEvent(eTouchMove);
 
     setTimeout(() => {
-      expect(res).to.be.ok;
+      expect(res).to.be.exist;
       done();
     }, 350);
   });
 
   it('bottom pull', (done) => {
-    let res = false;
+    let res;
     const vm = createVue({
       template: `
          <div style="height: 500px">
@@ -44,8 +44,8 @@ describe('event', () => {
       `,
       components: {PullTo},
       methods: {
-        onBottomPull() {
-          res = true;
+        onBottomPull(e) {
+          res = e;
         }
       }
     }, true);
@@ -60,9 +60,124 @@ describe('event', () => {
     elem.dispatchEvent(eTouchMove);
 
     setTimeout(() => {
-      expect(res).to.be.ok;
+      expect(res).to.be.exist;
       done();
     }, 350);
   });
 
+  it('top state change', (done) => {
+    let res;
+    const vm = createVue({
+      template: `
+         <div style="height: 500px">
+          <pull-to @top-state-change="onTopStateChange" :top-load-method="onTopLoadMethod"><div></div></pull-to>
+        </div>
+      `,
+      components: {PullTo},
+      methods: {
+        onTopLoadMethod() {},
+        onTopStateChange(e) {
+          res = e;
+        }
+      }
+    }, true);
+
+    let eTouchStart = createEvent('touchstart', true, true);
+    eTouchStart.touches = [{clientY: 0}];
+    let eTouchMove = createEvent('touchmove', true, true);
+    eTouchMove.touches = [{clientY: 60}];
+
+    const elem = vm.$el.querySelector('.scroll-container');
+    elem.dispatchEvent(eTouchStart);
+    elem.dispatchEvent(eTouchMove);
+
+    setTimeout(() => {
+      expect(res).to.be.exist;
+      done();
+    }, 20);
+  });
+
+  it('bottom state change', (done) => {
+    let res;
+    const vm = createVue({
+      template: `
+         <div style="height: 500px">
+          <pull-to @bottom-state-change="onBottomStateChange" :bottom-load-method="onBottomLoadMethod"><div></div></pull-to>
+        </div>
+      `,
+      components: {PullTo},
+      methods: {
+        onBottomLoadMethod() {},
+        onBottomStateChange(e) {
+          res = e;
+        }
+      }
+    }, true);
+
+    let eTouchStart = createEvent('touchstart', true, true);
+    eTouchStart.touches = [{clientY: 0}];
+    let eTouchMove = createEvent('touchmove', true, true);
+    eTouchMove.touches = [{clientY: -60}];
+
+    const elem = vm.$el.querySelector('.scroll-container');
+    elem.dispatchEvent(eTouchStart);
+    elem.dispatchEvent(eTouchMove);
+
+    setTimeout(() => {
+      expect(res).to.be.exist;
+      done();
+    }, 20);
+  });
+
+  it('infinite scroll', (done) => {
+    let res = false;
+    const vm = createVue({
+      template: `
+         <div style="height: 500px">
+          <pull-to @infinite-scroll="onInfiniteScroll"><div></div></pull-to>
+        </div>
+      `,
+      components: {PullTo},
+      methods: {
+        onInfiniteScroll(e) {
+          res = true;
+        }
+      }
+    }, true);
+
+    let event = createEvent('scroll', true, true);
+    const elem = vm.$el.querySelector('.scroll-container');
+    elem.dispatchEvent(event);
+
+    setTimeout(() => {
+      expect(res).to.be.ok;
+      done();
+    }, 420);
+  });
+
+  it('scroll', (done) => {
+    let res;
+    const vm = createVue({
+      template: `
+         <div style="height: 500px">
+          <pull-to @scroll="onScroll"><div></div></pull-to>
+        </div>
+      `,
+      components: {PullTo},
+      methods: {
+        onScroll(e) {
+          res = e;
+        }
+      }
+    }, true);
+
+    let event = createEvent('scroll', true, true);
+    const elem = vm.$el.querySelector('.scroll-container');
+    elem.dispatchEvent(event);
+
+    setTimeout(() => {
+      expect(res).to.be.exist;
+      done();
+    }, 200);
+  });
 });
